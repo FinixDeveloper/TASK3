9325f27882334cb97f7147d7e4e3887816ba2303
Praveen Kumar <142767317+FinixDeveloper@users.noreply.github.com>
2023-08-30 21:16:08 +0530

    Add files via upload

---
+++ Task3_3.js
@@ -0,0 +1,16 @@
+let xhr = new XMLHttpRequest();
+console.log(xhr);
+xhr.open("GET", "https://restcountries.com/v3.1/all");
+xhr.send();
+xhr.onload = function (){
+    const data = JSON.parse(xhr.response);
+    console.log(data);
+    for(let i=0; i<data.length; i++){
+        console.log(`
+            Name : ${data[i].name.common}
+            Region : ${data[i].region}
+            SubRegion : ${data[i].subregion}
+            Population : ${data[i].Population}
+            `);
+    }
+};
